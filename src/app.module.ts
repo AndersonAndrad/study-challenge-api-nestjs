8396e900e8e429b981871a88e9bb10503c555cc9
_Anderson_Andrade_ <31743641+AndersonAndrad@users.noreply.github.com>
2020-07-28 18:16:53 -0300

    importing my module into the project's root file
    adding the type module strainght from the typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+// configs
+import * as OrmOptions from './config/orm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(OrmOptions), RepoModule],
   controllers: [AppController],
   providers: [AppService],
 })
